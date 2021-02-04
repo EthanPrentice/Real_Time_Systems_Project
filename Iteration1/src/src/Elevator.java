07203f30f5f8752b5fe72922be771fb27364d2e0
nikhil-kharbanda <43783225+nikhil-kharbanda@users.noreply.github.com>
2021-02-04 17:53:18 -0500

    Elevator code fixed

--- Iteration1/src/src/Elevator.java
+++ Iteration1/src/src/Elevator.java
@@ -1,91 +1,43 @@
 package src;
 
+import src.adt.*;
+import java.lang.Thread;
+
 public class Elevator implements Runnable{     //Creates a new elevator
-	static int id;    //for additional elevators
-	static int CurrentFloor;
-	static int nextFloor;
-	static int direction = 2;
 	
-	public Elevator(int floor, int idNum) {
-		setCurrentFloor(floor);
-		id= idNum;
-	}
+	private Scheduler sch;
+
 	
-	//Running of the elevator motor
-	public void runMotor(int start, int end) {
-		int time = 2500;
-		if(start == end) {
-			System.out.println("Opening Doors...");
-			try {
-				Thread.sleep(time);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-				System.exit(1);
-			}
-		
-		} else { 
-			System.out.println("Closing Doors...");
-			try {
-				Thread.sleep(time);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-				System.exit(1);
-			}
-			
-			System.out.println("motor running...");
-			if (direction == 1) {
-				for (int i = start; i < end; i++) {
-					time = 800;
-					try {
-						Thread.sleep(time);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-						System.exit(1);
-					}
-					
-					System.out.println("Elevator traveling at floor " + (i+1));
-				}
-			} else if (direction == 0) {
-				for (int i = start; i > end; i--) {
-					time = 800;
-					try {
-						Thread.sleep(time);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-						System.exit(1);
-					}
-					
-					System.out.println("Elevator traveling at floor " + (i-1));
-				} 
-			}
-			System.out.println("Opening doors...");
-			time = 2500;
-			try {
-				Thread.sleep(time);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-				System.exit(1);
-			} 
-		}
-	}
-
-	public static int getCurrentFloor() {
-		return CurrentFloor;
-	}
-
-	public static void setCurrentFloor(int floor) {
-		CurrentFloor = floor;
-	}
-
-	public static int getNextFloor() {
-		return nextFloor;
-	}
-
-	public static void setNextFloor(int floor) {
-		nextFloor = floor;
+	public Elevator(Scheduler sch) {
+		this.sch = sch;
 	}
 	
 	public void run() {
-		Elevator e = new Elevator(1, 1);
+		boolean running = true;
+		while(running) {
+			
+			Event progress = sch.getEvent();
+			
+			System.out.println("Elevator: Received " + progress.toString() + " from scheduler...");
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				System.err.println(e.getMessage());
+				
+			}
+			
+			sch.sendEventToFloor(progress);
+			System.out.println("Elevator: Sending " + progress.toString() + " to scheduler...");
+			
+		
+		}
 	}
 }
+
+
+//send events to the sch
+
+//receive events from sch
+
+//call like how agent calls chefs
