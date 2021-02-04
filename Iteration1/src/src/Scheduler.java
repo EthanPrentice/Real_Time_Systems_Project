58ced7588cb0369e848b6749809baac0a04a544f
Baillie Noell <noellb797@gmail.com>
2021-02-04 16:56:23 -0500

    Update Scheduler.java

--- Iteration1/src/src/Scheduler.java
+++ Iteration1/src/src/Scheduler.java
@@ -3,17 +3,46 @@
  */
 package src;
 
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
 import src.adt.*;
 
 /**
  * @author noell
  *
  */
-public class Scheduler {
+public class Scheduler implements Runnable{
 	
-	public void put(FloorEvent event) {
-		// add to queue & notify this that event has been added to queue
+	private Queue<FloorEvent> eventQueue = new LinkedList<FloorEvent>();
+	private Floor floor;
+	
+	public Scheduler(Floor floor) {
+		this.floor = floor;
+	}
+	
+	public void putEventFromFloor(FloorEvent event) {
+		eventQueue.add(event);
+		System.out.println("Scheduler: Recieved event from Floor. Event: " + event.toString());
 		notify();
+	}
+	
+	public synchronized FloorEvent getEvent() {
+		while(eventQueue.isEmpty()) {
+			wait();
+		}
+		System.out.println("Scheduler: Sent event to Elevator. Event: " + eventQueue.peek().toString());
+		return eventQueue.remove();
+	}
+	
+	public void sendEventToFloor(FloorEvent event) {
+		System.out.println("Scheduler: Recieved Event From Elevator. Event: " + event.toString());
+		floor.put(event);
+	}
+
+	@Override
+	public void run() {
 	}
 
 }
