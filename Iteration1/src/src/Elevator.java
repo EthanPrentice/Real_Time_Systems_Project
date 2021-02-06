b37acfcd95f6996012e1d11dfa1d3f1ade17c234
sarahmadelia <sarah.abdallah99@outlook.com>
2021-02-06 14:10:43 -0500

    Update Elevator.java
    Added comments to Elevator code

--- Iteration1/src/src/Elevator.java
+++ Iteration1/src/src/Elevator.java
@@ -2,16 +2,26 @@ package src;
 
 import src.adt.*;
 import java.lang.Thread;
+/**
+ * Written for SYSC3303 - Group 6 - Iteration 1 @ Carleton University
+ * @author Nikhil Kharbanda
+ */
 
 public class Elevator implements Runnable{     //Creates a new elevator
 	
 	private Scheduler sch;
 
-	
+	/** 
+	 * Constructor 
+	 * @param sch Scheduler object
+	 */
 	public Elevator(Scheduler sch) {
 		this.sch = sch;
 	}
 	
+	/** 
+	 * Gets event from the Scheduler and then sends event back to the Scheduler
+	 */
 	public void run() {
 		boolean running = true;
 		while(running) {
@@ -34,10 +44,3 @@ public class Elevator implements Runnable{     //Creates a new elevator
 		}
 	}
 }
-
-
-//send events to the sch
-
-//receive events from sch
-
-//call like how agent calls chefs
